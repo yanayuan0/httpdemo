b249cf8a6bed27bba5d20645ace14dedfb00f5cd
yanayuan0 <149191649+yanayuan0@users.noreply.github.com>
2024-11-20 21:37:38 +0100

    Add files via upload

---
+++ app/src/main/java/hu/ait/httpdemo/network/MoneyAPI.kt
@@ -0,0 +1,17 @@
+package hu.ait.httpdemo.network
+
+import hu.ait.httpdemo.data.money.MoneyResult
+import retrofit2.http.GET
+
+// https://api.exchangerate-api.com/v4/latest/USD
+
+//Host: https://api.exchangerate-api.com/
+//Path: v4/latest/USD
+//Query params: we do not have now...
+
+interface MoneyAPI {
+
+    @GET("v4/latest/USD")
+    suspend fun getRates(): MoneyResult
+
+}
